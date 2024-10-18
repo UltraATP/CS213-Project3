385a56aafedad1af55e410a8774284ec410b27ff
Aarush Desai <aarushdesai64@gmail.com>
2024-10-17 21:31:15 -0400

    10/17

--- src/main/java/clinic/cs213project3/ClinicManagerController.java
+++ src/main/java/clinic/cs213project3/ClinicManagerController.java
@@ -1,6 +1,7 @@
 package clinic.cs213project3;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+// For button, remember to import the JavaFX version.
 
 /**
  * The controller class for managing what is displayed on the JavaFX UI.
@@ -13,12 +14,72 @@ public class ClinicManagerController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to the Clinic Manager!");
     }
 
-    @FXML
-    protected void tryButtonClick() {
-        welcomeText.setText("This is a test button!");
-    }
+    /**
+     * Commands...
+     *     * D
+     *         ** Schedule with Doctor.
+     *     * T
+     *         ** Scheduler with Technician.
+     *     * C
+     *         ** Cancel appointment.
+     *     * R
+     *         ** Reschedule appointment.
+     *     * PA
+     *         ** Print appointments by date.
+     *     * PP
+     *         ** Print appointments by patient.
+     *     * PL
+     *         ** Print appointments by county.
+     *     * PO
+     *         ** Print office appointments by county.
+     *     * PI
+     *         ** Print imaging appointments by county.
+     *     * PS
+     *         ** Print patient billing.
+     *     * PC
+     *         ** Print provider credits.
+     *     * Q
+     *         ** Quit program.
+     *
+     * JavaFX Utilities...
+     *     * TextField
+     *         * Def: Allow single-line user input.
+     *     * Button
+     *         * Def: A clickable control.
+     *     * RadioButton
+     *         * Def: Checkbox selection from a bunch of choices.
+     *     * TextArea
+     *         * Def: Allow multiple-line user input.
+     *     * TableView
+     *         * Def: Display data in a table.
+     *     * TabPane
+     *         * Def: Organize content into tabs.
+     *     * GridPane
+     *         * Def: Allows placement of UI elements in grid.
+     *
+     * Structure...
+     *     * Schedule Button (TabPane)
+     *         ** Office Appointment (RadioButton)
+     *             *** Inputs (TableView)
+     *         ** ImagingAppointment (RadioButton)
+     *             *** Inputs (TableView)
+     *     * Cancel Button (TabPane)
+     *         ** Inputs (TableView)
+     *     * Reschedule Button (TabPane)
+     *         ** Inputs (TableView)
+     *     * Appointments Button (TabPane)
+     *         ** Print Office Appointments (TabPane)
+     *         ** Print Imaging Appointments (TabPane)
+     *         ** Print All Appointments (TabPane)
+     *             *** PA (TabPane)
+     *             *** PP (TabPane)
+     *             *** PL (TabPane)
+     *     * Billing Button (TabPane)
+     *         ** Patient Billing (TabPane)
+     *         ** Provider Credits (TabPane)
+     */
 
 }
